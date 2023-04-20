de2480102468c27c457b6eb11f992d02eb39e6c0
IlsevNierop <ilsevannierop@gmail.com>
2023-04-20 11:50:11 +0200

    zo goed als klaar

--- src/app.js
+++ src/app.js
@@ -3,65 +3,63 @@ import axios from "axios";
 const countryList = document.getElementById("countries");
 const errorMessage = document.getElementById("error");
 
-// function getCountryColor(continent) {
-//     switch (continent) {
-//         case "Africa":
-//             continent = "blue";
-//             break;
-//         case "Asia":
-//             continent = "red";
-//             break;
-//         case "Europe":
-//             continent = "yellow";
-//             break;
-//         case "Oceania":
-//             continent = "purple";
-//             break;
-//         case "North America":
-//             continent = "green";
-//             break;
-//         case "South America":
-//             continent = "light-green";
-//             break;
-//         case "Antarctica":
-//             continent = "grey";
-//             break;
-//         default:
-//             continent = "black";
-//     }
-//     return continent;
-// }
+function switchRegionForColor(region) {
+    switch (region) {
+        case "Africa":
+            region = "blue";
+            break;
+        case "Asia":
+            region = "red";
+            break;
+        case "Europe":
+            region = "yellow";
+            break;
+        case "Oceania":
+            region = "purple";
+            break;
+        case "Americas":
+            region = "green";
+            break;
+
+        case "Antarctic":
+            region = "grey";
+            break;
+    }
+    return region;
+}
+
+function errorHandling(e) {
+    //errors afvangen in console
+    console.error(e);
+    //errors communiceren in de UI
+    if (e.response.status === 404) {
+        errorMessage.textContent = "Page not found | 404";
+    } else if (e.response.status === 500) {
+        errorMessage.textContent = "Internal server error | 500";
+    }
+}
 
 function mapCountryArrayInnerHTML(array) {
     array.map((country) => {
-        const continent = country.region;
-        // getCountryColor(continent);
         countryList.innerHTML +=
             `<li>
-                    <p>${country.flag}</p>
-                    <h4 class="${continent}">${country.name.common}</h4>
+                    <div class="row-country">
+                    <span>${country.flag}</span>
+                    <h4 class="${switchRegionForColor(country.region)}">${country.name.common}</h4>
+                    </div>
                     <p>Has a population of ${country.population} people</p>
                     </li>`;
     });
 }
 
-
 async function fetchCountries() {
     try {
-        const response = await axios.get("https://restcountries.com/v3.1/all?fields=flag,name,population,continents,region");
+        const response = await axios.get("https://restcountries.com/v3.1/all?fields=flag,name,population,region");
         const countryArray = response.data;
-        console.log(countryArray);
         countryArray.sort((a, b) => a.population - b.population);
         mapCountryArrayInnerHTML(countryArray);
     } catch (e) {
-        //errors afvangen in console
-        console.error(e);
-        //errors communiceren in de UI
-        if (e.response.status === 404) {
-            errorMessage.textContent = "Page not found | 404";
-        } else if (e.response.status === 500) {
-            errorMessage.textContent = "Internal server error | 500";
-        }
+        errorHandling(e);
     }
 }
 
